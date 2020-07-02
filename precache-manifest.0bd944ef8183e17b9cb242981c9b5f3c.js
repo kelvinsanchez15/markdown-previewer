30117365550b2a0df0a7a89db435f63284f4573e
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-07-01 22:31:14 -0400

    Updates

---
+++ precache-manifest.0bd944ef8183e17b9cb242981c9b5f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2179d9602308f9041def7e11301884",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "675b453e8e9dedad5def",
+    "url": "/markdown-previewer/static/css/main.01e730d6.chunk.css"
+  },
+  {
+    "revision": "867a84caa41e4214ffe8",
+    "url": "/markdown-previewer/static/js/2.f11d1f23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.f11d1f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675b453e8e9dedad5def",
+    "url": "/markdown-previewer/static/js/main.2abecc82.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
